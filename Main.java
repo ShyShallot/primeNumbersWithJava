0eb672c41c45f278b7a734513b3a4e6bc94568ce
ShyShallot <commanderpandayt@gmail.com>
2022-11-10 17:19:29 -0800

    added user input

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     numHandler numcount = new numHandler();
@@ -9,9 +10,18 @@ class Main {
     //System.out.println(isPrime(24));
     //System.out.println(numcount.countPrimes(24));
     //System.out.println(numcount.countPrimes(100));
-    System.out.println(numcount.countPrimes(1000000, 1000));
+    //System.out.println(numcount.countPrimes(1000000, 1000));
     //System.out.println(numcount.countPrimesEff(1000000, 1000));
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a Number");
+    int num = input.nextInt();
+    input.close();
+    System.out.println("Factors of " + num + ": " + numcount.countFactors(num));
+    boolean isPrime = numcount.isPrime(num);
+    System.out.println("Is " + num + " prime? " + isPrime);
+    System.out.println("Prime Factors between 2 and " + num + ": " + numcount.countPrimes(num).get("primeNumbers"));
 
+    
   }
   
 
--- numHandler.java
+++ numHandler.java
@@ -20,55 +20,26 @@ public class numHandler{
     return false;
   }
 
-  public HashMap<String,Integer> countPrimes(int num, int printStep){ // check countPrimesEff for comments
+  public HashMap<String,Integer> countPrimes(int num){ // check countPrimesEff for comments
     //System.out.println(Arrays.toString(primeNum));
-    HashMap<String, Integer> primes = new HashMap<>();
-    long start = System.currentTimeMillis();
+    progressbar progress = new progressbar();
+    HashMap<String, Integer> primes = new HashMap<>(); // create a map of all our prime numbers
+    long start = System.currentTimeMillis(); // keep track of what time we started the calculation
     int numOfPrimes = 0;
-    for(int i = 2;i<num;i++){
-      long start2 = System.currentTimeMillis();
+    for(int i = 2;i<num;i++){ // starting from 2 to our input
      // System.out.println("Checking if " + i + " is Prime");
       if(isPrime(i)){
         //System.out.println(i + " is prime");
         //System.out.println(Arrays.toString(primeNum));
-        primes.put(String.valueOf(i),i);
+        primes.put(String.valueOf(i),i); // insert prime number into our map
         numOfPrimes++;
       }
-      if(i % printStep == 0){
-        long time = System.currentTimeMillis() - start2;
-        System.out.println(i + " Calc Took: " + time + "ms");
+      if(num >= 100000 && (i % 1000 == 0)){
+        System.out.println(i);
       }
     }
     primes.put("primeNumbers", numOfPrimes);
     System.out.println("Final Calc Took: " + (System.currentTimeMillis() - start) + "ms");
     return primes;
-  }
-// The reason for this being a different function is that the design requirements for the base countPrimes function is that it had to call the other 2 functions, and i wanted to see if this was faster.
-  public HashMap<String,Integer> countPrimesEff(int num, int printStep){ 
-    long start = System.currentTimeMillis();  // Base Start time for total calculations
-    HashMap<String, Integer> primes = new HashMap<>(); // create hash map to store calculated prime values
-    int numOfPrimes = -1; // offset by 1 as for some reason this function is inaccurate by 1.
-    for(int i = num; i>0;i--){ // start from our input number and work down as this gets slower the bigger the number
-      long start2 = System.currentTimeMillis(); // start time for each number
-      int numOfFac = 0; // Number of Factors for the number I in our for loop
-      for(int y = 1; y<=i;y++){ // loop through each number to I starting from 1 and see if its a factor of that number
-        if(i % y == 0){ // if there is no remainder Y is a factor of I
-          numOfFac++;
-        }
-      }
-      //System.out.println(numOfFac + ", " + i);
-      if(numOfFac <= 2){ // if the number I has no other factor other than 1 and its self.
-        //System.out.println(i + " is a prime number");
-        primes.put(String.valueOf(i), i); // enter prime number into HashMap
-        numOfPrimes++; // add to number of primes
-      }
-      if(i % printStep == 0){ // This is just to print every printStep, so ie every 100, 1000 and so on numbers, as printing every number and timestamp slows it down but we still need a marker of progress, give or take its about 100ms difference
-        long time = System.currentTimeMillis() - start2;
-        System.out.println(i + " Calc Took: " + time + "ms");
-      }
-    }
-    System.out.println("Final Calc Took: " + (System.currentTimeMillis() - start) + "ms");
-    primes.put("primeNumbers",numOfPrimes);
-    return primes; // return our hashmap
   }
 }
